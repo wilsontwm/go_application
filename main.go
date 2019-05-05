f88c3f2aa551d00cc91b554c22d286b50a6b86ee
wilsontanwm <wilson.tan@traitquest.com>
2019-05-05 23:42:19 +0800

    Added scan template files functionality

--- main.go
+++ main.go
@@ -6,10 +6,46 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"html/template"
+	"path/filepath"
 )
+
+var viewPath = "views"
+var templates *template.Template
+
+func getTemplates() (templates *template.Template, err error) {
+	var allFiles []string
+	
+	// Loop through all the files in the views folder including subfolders
+	err = filepath.Walk(viewPath, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			allFiles = append(allFiles, path)
+		} 
+
+		return nil
+	})
+
+	if err != nil {
+		log.Print("Error walking the file path", err)
+	}
+
+	templates, err = template.New("").ParseFiles(allFiles...)
+	
+	if err != nil {
+		log.Print("Error parsing template files", err)
+	}
+
+    return
+}
+
+func init() {
+    templates, _ = getTemplates()
+}
 
 func main() {
 	router := mux.NewRouter()
+	// Routes
+	router.HandleFunc("/", hello).Methods("GET")
 
 	port := os.Getenv("port")
 	if port == "" {
@@ -18,8 +54,17 @@ func main() {
 
 	fmt.Println("Server started and running at port", port)
 
-	err := http.ListenAndServe(":" + port, router)
+	log.Fatal(http.ListenAndServe(":" + port, router))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{
+		"title": "Homepage",
+	}
+
+	err := templates.ExecuteTemplate(w, "home_html", data)
+
 	if err != nil {
-		log.Print("Server running error: ", err) 
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
